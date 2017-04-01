6bf1c8beaba248667de6935b86462278df67f565
Alyssa-Santo <alyssasantopadre@gmail.com>
2017-04-01 13:39:08 -0400

    Add files via upload

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'YvwtYePobSdyS5LIflAYAhI2W',
+  consumer_secret: 'u3N27gMc0FIy5tUxI53xlhOzCBifbRdIH1TVHkc2DyiAzHLPhy',
+  access_token_key: '631603448-Hs7oU7rDS2Cdipn71twDUrS4sMxsgBER804FfkPw',
+  access_token_secret: 'EsFe4DegkUXyj1TgMe9TokjmBMQVuc8yfJ7csz6ScPG9D'
+};
